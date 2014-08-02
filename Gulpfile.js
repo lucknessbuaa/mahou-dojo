1f54a1980b577bcba89b2d6668ea344296e9a085
Jarvys <yuhan534@126.com>
2014-08-02 10:46:28 +0800

    minify images

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,10 @@ var sass = require("gulp-sass");
 var minifyCSS = require('gulp-minify-css');
 var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
+var pngcrush = require("imagemin-pngcrush");
+var tinypng = require("gulp-tinypng");
 var nodemon = require('gulp-nodemon');
+var changed = require('gulp-changed');
 
 gulp.task('sass', function() {
     gulp.src("scss/*.scss")
@@ -27,3 +30,23 @@ gulp.task('develop', function() {
         console.log('restarted!')
     })
 })
+
+gulp.task('image-png', function() {
+    return gulp.src("img/*.png")
+        .pipe(changed('public/img'))
+        .pipe(tinypng('9kl3nT2f8qC-AaApBVXDeQt-37ArLMNs'))
+        .on('error', console.error)
+        .pipe(gulp.dest("public/img"));
+});
+
+gulp.task('image-other', function() {
+    return gulp.src("img/*.{jpg,jpeg,gif}")
+        .pipe(changed('public/img'))
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest("public/img"));
+});
+
+gulp.task('image', ['image-png', 'image-other']);
