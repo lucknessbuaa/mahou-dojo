bc66778dcd16d2bcfca3464ff3af1a7a76bd50fa
yangchen <yuhan534@126.com>
2014-08-05 14:32:33 +0800

    save

--- bin/update.js
+++ bin/update.js
@@ -19,7 +19,7 @@ MongoClient.connect("mongodb://localhost:27017/mahou", function(err, db) {
         var CURRENT_ROUND = 0;
 
         var _date = function(minutes, seconds) {
-            var date = datetime(2014, 8, 5, 11, 36, 00);
+            var date = datetime(2014, 8, 5, 14, 2, 00);
 			seconds = seconds || 0;
 			minutes = minutes || 0;
             return new Date(date.getTime() + minutes * 1000 * 60 + seconds * 1000);
@@ -31,7 +31,7 @@ MongoClient.connect("mongodb://localhost:27017/mahou", function(err, db) {
             magicians: [{
                 first: true,
                 id: 1,
-                name: '陈志鹏 / 丁建中',
+                name: '丁建中 / 陈志鹏',
                 start: _date(0, 30),
                 avatar: '/img/magician-avatar-1.png',
                 score: _date(0, 45),
@@ -40,46 +40,46 @@ MongoClient.connect("mongodb://localhost:27017/mahou", function(err, db) {
             }, {
                 id: 2,
                 name: '阿牛 / 郭汛杰',
-                start: _date(1, 30),
+                start: _date(1, 10),
                 avatar: '/img/magician-avatar-2.png',
-                score: _date(1, 45),
-                end: _date(2, 0),
+                score: _date(1, 25),
+                end: _date(1, 45),
                 scores: [Show.THREE, Show.KING, Show.ACE]
             }, {
                 id: 3,
                 name: '凯特琳 / 杨洪基',
-                start: _date(2, 30),
+                start: _date(2, 00),
                 avatar: '/img/magician-avatar-3.png',
-                score: _date(2, 45),
-                end: _date(3, 0),
+                score: _date(2, 15),
+                end: _date(2, 30),
                 scores: [Show.FIVE, Show.SEVEN, Show.THREE]
             }, {
                 id: 4,
                 name: '高虎 / 王子妃',
-                start: _date(3, 30),
+                start: _date(2, 45),
                 avatar: '/img/magician-avatar-4.png',
-                score: _date(3, 45),
-                end: _date(4, 0),
+                score: _date(3, 0),
+                end: _date(3, 15),
                 scores: [Show.SEVEN, Show.ACE, Show.JACK]
             }, {
                 id: 5,
                 name: '谭永铨 / 叶童',
-                start: _date(4, 30),
+                start: _date(3, 30),
                 avatar: '/img/magician-avatar-5.png',
-                score: _date(4, 45),
-                end: _date(5, 0),
+                score: _date(3, 45),
+                end: _date(4, 0),
                 scores: [Show.NINE, Show.JACK, Show.SEVEN]
             }, {
                 id: 6,
                 name: '萧蔷 / 傅琰东',
-                start: _date(5, 30),
+                start: _date(4, 15),
                 avatar: '/img/magician-avatar-6.png',
-                score: _date(5, 45),
-                end: _date(6, 0),
+                score: _date(4, 30),
+                end: _date(4, 45),
                 scores: [Show.JACK, Show.NINE, Show.FIVE]
             }],
-            score: _date(6, 3),
-            end: _date(7, 30),
+            score: _date(4, 47),
+            end: _date(5, 0),
         };
 
         collection.update({
